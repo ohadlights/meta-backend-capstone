f180646c5ff2d52b515af6a6e67d5af188551dc8
ohad <ohad.meir@intel.com>
2025-01-17 15:53:17 +0200

    restaurant index view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
-def sayHello(request):
-    return HttpResponse('Hello World')
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
