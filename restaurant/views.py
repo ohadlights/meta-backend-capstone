2ff794a491949627b658108c46a60ca73e0fe95f
ohad <ohad.meir@intel.com>
2025-01-19 21:40:38 +0200

    restaurant/views.py - for booking tables

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
-from .models import Menu
-from .serializers import MenuSerializer
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
 
 # Create your views here.
 def index(request):
@@ -15,3 +18,8 @@ class MenuItemView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
