f8f1ab415c1c5de1fe735bd7c4bf081524ee1135
ohad <ohad.meir@intel.com>
2025-01-19 21:26:09 +0200

    restaurant/views.py - add Menu views

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurant.apps.RestaurantConfig',
+    'rest_framework',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
-
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+
+from .models import Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
