1569c45be55884d312d8879ac16a4cc294ffca6a
ohad <ohad.meir@intel.com>
2025-01-21 21:13:38 +0200

    littlelemon/settings.py - wrong password for tests?

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
